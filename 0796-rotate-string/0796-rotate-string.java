508dd6cd8038152def0ed64c47db3d141dc37175
Shuvam siddheswar sahoo <144039159+Shuvamsahoo123@users.noreply.github.com>
2026-08-25 17:52:14 +0530

    Time: 3 ms (6.85%), Space: 43.2 MB (83.01%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,8 +1,19 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() == goal.length() && (s+s).contains(goal)){
-            return true;
+        if(s.length() != goal.length() ){
+            return false;
         }
-        else return false;
+
+        String current = s;
+        for( int i = 0; i< s.length() ; i++){
+            if(current.equals(goal)){
+                return true;
+            }
+
+            current = current.substring(1)+current.charAt(0);
+        }
+
+        return false;
+
     }
 }
